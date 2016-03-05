214a02d01decafd63838174a39bc1177c0db7eb3
Joseph Devita <jd761@cp.cs.rutgers.edu>
2016-03-04 20:53:49 -0500

    * added typedefs to signal.h

--- signal.h
+++ signal.h
@@ -7,4 +7,14 @@
 // At a minimum you must define the signal constants themselves
 // as well as a sighandler_t type.
 
+typedef struct
+{
+  int signum;
+} siginfo_t;
+
+typedef void (*sighandler_t)(siginfo_t);
+
+#define SIGFPE 0
+#define SIGALRM 1
+
 #endif
