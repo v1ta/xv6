76890aacb917165d854a6a743013f4a7ef94879d
v1ta <jodvita@gmail.com>
2016-03-06 15:46:50 -0500

    more files

--- proc.h
+++ proc.h
@@ -68,6 +68,7 @@ struct proc {
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
   int *handlers[24]; 
+  int *restorer;
 };
 
 // Process memory is laid out contiguously, low addresses first:
